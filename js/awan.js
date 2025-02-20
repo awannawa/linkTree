df8d5f3856c71b89aea6f2354905aa76f35f8a13
awannawa <kurniawanofficiall@gmail.com>
2025-02-20 17:46:46 +0700

    change to db sheet

--- js/awan.js
+++ js/awan.js
@@ -1,3 +1,5 @@
+var loading = document.getElementById("loading"); // Loading spinner element
+
 function updateImages() {
   const images = document.querySelectorAll(".gdrive-image");
   images.forEach(function (img) {
@@ -8,3 +10,30 @@ function updateImages() {
 
 // Event listener untuk memanggil fungsi updateImages saat window load
 window.addEventListener("load", updateImages);
+
+loading.style.display = "flex";
+fetch(
+  "https://script.googleusercontent.com/macros/echo?user_content_key=sbACTqH27Tmh-YgUulb5jlkpxK-rcZcOab2oEXXwz22LI2V3066lYgaDnjzRdQ4kaFX4W2xEVxphBVHv-5AM-3SWRYogs7vPm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnBxqfxbtQcOPFdYQQArrpGG2IDj4IeEhgDVsgs0UzpMH3-hk5QHcELt1NNrk5GnmE3iTdyo4g0TMYo5Grr5dW3i6Rq62vnBhCg&lib=MbTCY94-B3SGcli0nfwpjAW9v2Zxi0IEI"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    updateLinks(data);
+  });
+
+function updateLinks(data) {
+  let linksContainer = document.getElementById("link-page");
+
+  data.forEach((item) => {
+    let linkElement = document.createElement("a");
+    linkElement.href = item["link-page"];
+    linkElement.target = "_blank";
+
+    let iconElement = document.createElement("i");
+    iconElement.className = item["font-awesome"];
+
+    linkElement.appendChild(iconElement);
+    linkElement.appendChild(document.createTextNode(` ${item["name-page"]}`));
+    linksContainer.appendChild(linkElement);
+    loading.style.display = "none";
+  });
+}
